10f4dc12ab24533070ff449abed19205a3c22037
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-09-03 20:57:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "batch10-dev-tf-state-bucket"
+    bucket = "batch11-dev-tf-state-bucket"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "batch10-dynamo-table"
