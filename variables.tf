05741eaddf5de59d08b172ec05be853d3086b6b7
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-09-03 21:30:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,51 +1,33 @@
-variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+variable "AWS_ACCESS_KEY" {
+  type = string
+  default = "AKIAWZQUPLR3UJFZPGV6"
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "ansiblekey" 
+variable "AWS_SECRET_KEY" {
+    default = "63wrKZ+5CFXrv/svk+s/gB8ZzWq3JHiyx4rrXzge"
+  
 }
 
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "AWS_REGION" {
+    default = "ap-south-1"
+  
 }
 
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "jenkins-sgroup-sep2023" 
-}
 
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "suresh" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0f5ee92e2d63afc18" 
-}
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
-}
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
-}
-variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
+variable "ami" {
+    type = map
+    default = {
+        us-east-1 = "ami-06a1f46caddb5669e"
+        us-east-2 = "ami-00280fb6ed84811ce"
+        us-west-2 = "ami-022c9f1a24f813bf9"
+        us-west-1 = "ami-075138a50b1af6e68"
+        ap-south-1 = "ami-0e7e9cf689200b454"
+
     }
+  
+}
+
+variable "inst-type" {
+    default = "t2.micro"
+  
 }
