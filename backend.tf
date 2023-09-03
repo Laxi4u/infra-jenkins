0c120c2c69277713d4d3ed32f32e3d43bc30c815
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-09-03 21:25:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "batch11-dev-tf-state-bucket"
-    key = "main"
-    region = "ap-south-1"
-    dynamodb_table = "batch10-dynamo-table"
-  }
-}
+#terraform {
+  #backend "s3" {
+    #bucket = "batch11-dev-tf-state-bucket"
+    #key = "main"
+    #region = "ap-south-1"
+    #dynamodb_table = "batch10-dynamo-table"
+  #}
+#}
