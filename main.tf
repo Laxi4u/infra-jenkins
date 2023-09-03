f9fce1c42baefe1451c0407e557c7ba00ceda45d
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-09-03 22:04:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "test-ec2" {
     ami = lookup(var.ami, var.AWS_REGION)
     instance_type = var.inst-type
     #key_name = aws_key_pair.test-key-pair.key_name
-    key_name = "kops-new"
 
     vpc_security_group_ids = [aws_security_group.test-sg.id]
     subnet_id = aws_subnet.test-public-subnet.id
